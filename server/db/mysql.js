921c79515f688e494e46372188949014fd9ae8a7
guohao <631187563@qq.com>
2020-08-27 18:22:49 +0800

    mysql config 修改

--- server/db/mysql.js
+++ server/db/mysql.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql");
-const { MYSQL_CONFIG } = require("..config");
+const { MYSQL_CONFIG } = require("../config");
 const pool = mysql.createPool(MYSQL_CONFIG);
 // pool.query("SELECT * FROM users", function (error, results, fields) {
 //     if (error) throw error;
@@ -7,7 +7,7 @@ const pool = mysql.createPool(MYSQL_CONFIG);
 //     console.log("fields are : ", fields);
 // });
 // 执行 sql 方法
-const executeSql = sql => {
+const executeSql = (sql) => {
     return new Promise((resolve, reject) => {
         pool.query(sql, (error, result) => {
             if (error) {
@@ -19,11 +19,19 @@ const executeSql = sql => {
 };
 
 // executeSql("SELECT * FROM ausers")
-//     .then(result => {
+//     .then((result) => {
 //         console.log("The solution is: ", result[0]);
 //     })
-//     .catch(err => {
+//     .catch((err) => {
 //         console.log(err.sqlMessage);
 //     });
+// const cityList = ["深圳", "厦门", "西藏", "河南", "天津", "北京", "南京", "西京", "东京"];
+// for (let i = 0; i < 1000; i++) {
+//     const random = Math.floor(Math.random() * 9);
+//     const id = Math.floor(Math.random() * i * 10);
+//     const city = cityList[random];
+//     const sql = `INSERT INTO city_memory(city, country_id) VALUES('${city}',${id})`;
+//     executeSql(sql);
+// }
 
 module.exports = executeSql;
